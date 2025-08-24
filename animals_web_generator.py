6558906816c223a9403b7d5a303749973f32df6b
Zahra Rauf <zahra.aleem@gmail.com>
2025-08-24 15:03:40 +0200

    Bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,12 @@
 import json
 
+def read_HTML(file_path):
+  """ Reads the data from the HTML file"""
+  with open(file_path, "r") as handle:
+    return handle.read()
+
+old_HTML = read_HTML("animals_template.html")
+
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
@@ -7,20 +14,35 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-print(len(animals_data))
+#print(len(animals_data))
 
+# to store the HTML text
+HTML_text = ""
+
+# To generate the display info from the json file
 for animal in animals_data:
   try:
-    name = animal["name"]
-    print(f"Name: {name}")
-    diet = animal["characteristics"]["diet"]
-    print(f"Diet: {diet}")
-    location = animal["locations"][0]
-    print(f"Location: {location}")
-    typ = animal["characteristics"]["type"]
-    print(f"Type: {typ}")
-    print()
+    # Appending data to the HTML string
+    HTML_text += f"Name: {animal["name"]}\n"
+
+    HTML_text += f"Diet: {animal["characteristics"]["diet"]}\n"
+
+    HTML_text += f"Location: {animal["locations"][0]}\n"
+
+    HTML_text += f"Type: {animal["characteristics"]["type"]}\n"
+    HTML_text += "\n"
 
   except KeyError:
-    print()
+    HTML_text += "\n"
+
+#print(HTML_text)
+
+new_HTML = old_HTML.replace("__REPLACE_ANIMALS_INFO__", HTML_text)
+
+# Creating a new HTML file with the new HTML text
+with open("animals.html", "w") as animal_HTML:
+  animal_HTML.write(new_HTML)
+
+
+
 
